35548095bc1388ec6425115e60120be877af1e9e
Hossein.Krouna <hossein.krouna@gmx.net>
2025-06-28 19:17:09 +0200

    add: 404 player page

--- detail.php
+++ detail.php
@@ -7,12 +7,16 @@ $player = filter_input(INPUT_GET, 'player', FILTER_VALIDATE_REGEXP,
  ['options' => ['regexp' => '/^[\w\s\-\'\:]+$/']]);
 
 
-if($player) {
-$one_player = get_player($player);
-}else{
-    //TODO: 404
+if(!$player) {
+    redirect("index.php");
 }
 
+$one_player = get_player($player);
+
+if(!$one_player) {
+    view('404');
+    die();
+}
 
 $view_data = [
     'title' => 'Detailansicht',
